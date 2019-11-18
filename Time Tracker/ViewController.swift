9c15493ca5ddffd262e8c3b0f3d61d21c69df66a
Wladmir Júnior <wladmir.silva@bancointer.com.br>
2019-11-18 19:20:41 -0300

    Initial Commit

---
+++ Time Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Tracker
+//
+//  Created by Wladmir Edmar Silva Junior on 18/11/19.
+//  Copyright © 2019 Wladmir Edmar Silva Junior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
